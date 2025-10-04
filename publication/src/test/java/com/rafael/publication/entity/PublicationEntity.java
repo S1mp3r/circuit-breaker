b060aab0d20b9ed3931a3ec0a04abeacb0c4af59
S1mp3r <rafaelcarneiro19@edu.unifor.br>
2025-10-04 12:10:08 -0300

    feat(entity): adding our mongo entity

---
+++ publication/src/test/java/com/rafael/publication/entity/PublicationEntity.java
@@ -0,0 +1,18 @@
+package com.rafael.publication.entity;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.MongoId;
+
+import lombok.Data;
+
+@Data
+@Document(collection = "publications")
+public class PublicationEntity {
+    
+    @MongoId
+    private String id;
+    private String title;
+    private String imageURL;
+    private String text;
+
+}
