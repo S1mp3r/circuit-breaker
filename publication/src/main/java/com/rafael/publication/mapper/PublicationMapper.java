5843c5ca802654900909410453350b707a58c289
S1mp3r <rafaelcarneiro19@edu.unifor.br>
2025-10-04 13:06:41 -0300

    fix(Mapper): fixing a issue with pom being outdated and also mapper not considering the fiels properly

--- publication/src/main/java/com/rafael/publication/mapper/PublicationMapper.java
+++ publication/src/main/java/com/rafael/publication/mapper/PublicationMapper.java
@@ -1,6 +1,7 @@
 package com.rafael.publication.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import com.rafael.publication.domain.Publication;
 import com.rafael.publication.entity.PublicationEntity;
@@ -9,7 +10,12 @@ import com.rafael.publication.model.PublicationRequest;
 @Mapper(componentModel = "spring")
 public interface PublicationMapper {
     
+    @Mapping(target = "id", ignore = true)
     PublicationEntity toPublicationEntity(Publication publication);
+    
+    @Mapping(target = "comments", ignore = true)
     Publication toPublication(PublicationEntity publication);
+
+    @Mapping(target = "comments", ignore = true)
     Publication toPublication(PublicationRequest publication);
 }
